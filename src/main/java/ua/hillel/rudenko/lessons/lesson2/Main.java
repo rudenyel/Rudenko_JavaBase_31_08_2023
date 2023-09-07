ec663a75314bb0afa8ce94e487a10028952a7915
Yelyzaveta Rudenko <murkaliza@gmail.com>
2023-09-07 19:16:12 +0200

    commit from idea

--- src/main/java/ua/hillel/rudenko/lessons/lesson2/Main.java
+++ src/main/java/ua/hillel/rudenko/lessons/lesson2/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args){
         double latitude = 50.089749641;
         double longitude = 14.3983984064;
+        //
         System.out.println("Latitude: " + latitude + " Longitude " + longitude);
         char symbol1 = '\u00B0';
         System.out.println("DMS latitude: 50" + symbol1 + " 05'23.10\" N   DMS latitude: 14" + symbol1 + "23'54.23\" E");
---
+++ src/main/java/ua/hillel/rudenko/lessons/lesson3/Main.java
@@ -0,0 +1,4 @@
+package ua.hillel.rudenko.lessons.lesson3;
+
+public class Main {
+}
